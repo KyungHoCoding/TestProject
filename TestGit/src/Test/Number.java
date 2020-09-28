bfc7de0fe4697a32ede37fb6adcbc37d7cda383c
KyungHoCoding <71607215+KyungHoCoding@users.noreply.github.com>
2020-09-28 11:56:28 +0900

    숫자

--- TestGit/src/Test/Number.java
+++ TestGit/src/Test/Number.java
@@ -4,7 +4,10 @@ public class Number {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a = 1;
+		System.out.println(a+1);
+		String b ="±êÇãºê Ä¿¹Ô";
+		System.out.println(b+"¿Ï·á");
 	}
 
 }
